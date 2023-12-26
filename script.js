0ac44337769d8be82b1a219b1501f8eac1df8d20
cdc-001 <cory.caddell@gmail.com>
2023-12-25 20:18:14 -0600

    add hover effect. fix grid

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-/******** Create grid tiles **************/
+/******** Create grid  **************/
 const gridDimension = 16;
 const gridContainer = document.querySelector('.gridContainer');
 
 for (let i = 0; i < gridDimension; i++) {
     
-    let rows = document.createElement('div');
-    rows.classList.add('tile'); 
+    let col = document.createElement('div');
+    col.classList.add('container');
     
     for (let j = 0; j < gridDimension; j++) {
-        let col = document.createElement('div');
-        col.classList.add('tile');
-        rows.appendChild(col);
+        let row = document.createElement('div');
+        row.classList.add('tile');
+        col.appendChild(row);
     }
     
-    gridContainer.appendChild(rows);
+    gridContainer.appendChild(col);
 }
+
+/********* Create hover effect ********/
+const allTiles = document.querySelectorAll('.tile')
+allTiles.forEach( (tile) => {
+    tile.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = 'black';
+    });
+});
